9749f9804ced47cee0277f7deb6273930fbaee87
Elodie663 <elodie.molieres14@gmail.com>
2025-08-07 20:37:54 +0200

    Modifications de l'index html et début du script JS

---
+++ script.js
@@ -0,0 +1,19 @@
+let tableauKanban = [
+  //tableau global pour définir les taches
+  {
+    nomListe: "À faire",
+    taches: ["HTML", "Style CSS", "Script JS"],
+  },
+  {
+    nomListe: "En cours",
+    taches: ["paramétrer GitHub"],
+  },
+  {
+    nomListe: "Terminé",
+    taches: ["Valider la maquette"],
+  },
+  {
+    nomListe: "Approuvé",
+    taches: [""],
+  },
+];
